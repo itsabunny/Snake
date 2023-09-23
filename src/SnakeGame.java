75b7c9f0960a37670c7319239bfbd9e80ea4d8bc
Ninis <itsabunny@pm.me>
2023-09-23 11:35:49 +0200

    Game Over and collision with itself

--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -31,6 +31,7 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
     Timer gameLoop;
     int velocityX;
     int velocityY;
+    boolean gameOver = false;
 
     SnakeGame(int boardWidth, int boardHeight) {
         this.boardWidth = boardWidth;
@@ -115,12 +116,25 @@ public class SnakeGame extends JPanel implements ActionListener, KeyListener {
         // Snake Head
         snakeHead.x += velocityX;
         snakeHead.y += velocityY;
+
+        // game over conditions
+        for (int i = 0; i < snakeBody.size(); i++) {
+            Tile snakePart = snakeBody.get(i);
+            // collide with the snake head
+            if (collision(snakeHead, snakePart)) {
+                gameOver = true;
+            }
+        }
+
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         move();
         repaint();
+        if (gameOver) {
+            gameLoop.stop();
+        }
     }
 
     @Override
